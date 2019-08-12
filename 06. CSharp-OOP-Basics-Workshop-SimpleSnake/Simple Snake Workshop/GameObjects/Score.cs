978cc9588cbb56bc981917b5f6c773e181c90ad1
Georgi Terziev <terzata2002@abv.bg>
2019-08-12 13:44:09 +0300

    Added Score

---
+++ 06. CSharp-OOP-Basics-Workshop-SimpleSnake/Simple Snake Workshop/GameObjects/Score.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SimpleSnake.GameObjects
+{
+    public class Score
+    {
+        private int leftX;
+        private int topY;
+        public Score(int leftX, int topY)
+        {
+            this.leftX = leftX;
+            this.topY = topY;
+
+            this.Points = 0;
+        }
+        public int Points { get; private set; }
+
+        public void IncreasePoints(int food)
+        {
+            this.Points += food + 1;
+        }
+        public void PrintPoints()
+        {
+            Console.SetCursorPosition(leftX, topY);
+            Console.WriteLine("Your score:");
+            Console.SetCursorPosition(leftX, topY + 1);
+            Console.WriteLine(this.Points);
+            Console.SetCursorPosition(leftX, topY + 2);
+        }
+    }
+}
